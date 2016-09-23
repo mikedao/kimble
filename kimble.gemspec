05f726f9602ad4e8c0d4bbc4e0cd153ad23cd56e
Michael Dao <michael.dao@gmail.com>
2016-09-23 12:36:05 -0600

    Adds additional quotes

---
+++ kimble.gemspec
@@ -0,0 +1,12 @@
+Gem::Specification.new do |s|
+  s.name        = 'kimble'
+  s.version     = '0.0.2'
+  s.date        = '2016-09-23'
+  s.summary     = "I'll be back"
+  s.description = "Get a random Arnold Schwarzenegger quote."
+  s.authors     = ["Michael Dao"]
+  s.email       = ["michael.dao@gmail.com"]
+  s.files       = ["lib/kimble.rb"]
+  s.homepage    = 'http://rubygems.org/gems/kimble'
+  s.license     = 'MIT'
+end
--- lib/kimble.rb
+++ lib/kimble.rb
@@ -4,13 +4,37 @@ class Kimble
     self.quotes_list.shuffle.first
   end
 
-  def quotes_list
+  def self.quotes_list
     [
      "Who is your daddy and what does he do?",
      "I'm a cop you idiot.",
      "Get to the choppa!",
      "I'll be back.",
-     "Hasta la vista, baby."
+     "Hasta la vista, baby.",
+     "To crush your enemies, see them driven before you, and to hear
+     the lamentation of their women!",
+     "Your clothes, give them to me now!",
+     "Do it!",
+     "If it bleeds, we can kill it",
+     "See you at the party, Richter!",
+     "Let off some steam, Bennett",
+     "Oh, cookies! I can't wait to toss them.",
+     "Someone you trust wants to talk to you.",
+     "Who is it this time, my mother?",
+     "If you yield only to a conqueror, then prepare to be conquered.",
+     "My name is Julius and I'm your twin brother",
+     "What killed the dinosaurs? The Ice Age!",
+     "They're six year olds, how much trouble can they be?",
+     "Capitalism!",
+     "No more complaining. No more 'Mr. Kimble, I have to go the bathroom'. Nothing! There is no bathroom!",
+     "I'm the party pooper.",
+     "Come with me if you want to live.",
+     "No, I am not shitting you.",
+     "Stop whining! You kids are soft. You lack discipline.",
+     "Fuck YOU, asshole!",
+     "A freeze is coming!",
+     "Get your ass to Mars.",
+     "Are you Sarah Connor?"
     ]
   end
 
